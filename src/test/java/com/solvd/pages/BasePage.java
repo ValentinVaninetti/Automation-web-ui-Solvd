06b2f02eebca7aacd3f0ab14da4645dbc5def789
jjuanpg <juanpablogordillo375@gmail.com>
2022-10-20 10:35:49 -0300

    update

--- src/test/java/com/solvd/pages/BasePage.java
+++ src/test/java/com/solvd/pages/BasePage.java
@@ -3,8 +3,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
-
-
 public abstract class BasePage {
     protected WebDriver webDriver;
     protected WebDriverWait webDriverWait;
